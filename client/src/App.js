f866e3ede228f2ca7f8871a8d249255fa1affd3f
Qibin <qbye0405@gmail.com>
2022-08-09 11:49:33 -0400

    added tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,30 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((r) => r.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    // <BrowserRouter>
+    //   <div className="App">
+    //     <Switch>
+    //       <Route path="/testing">
+    //         <h1>Test Route</h1>
+    //       </Route>
+    //       <Route path="/">
+    //         <h1>Page Count: {count}</h1>
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </BrowserRouter>
   );
 }
 
